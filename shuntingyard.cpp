2d96d804967a1f9ab341b1c31922bfff3563ffd3
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2023-03-07 12:18:59 -0800

    Update shuntingyard.cpp

--- shuntingyard.cpp
+++ shuntingyard.cpp
@@ -3,7 +3,36 @@
 
 using namespace std;
 
+void pushStack(Node* newNode);
+Node* popStack();
+void peekStack();
+enqueueQueue();
+dequeueQueue();
+
+Node* stackHead = NULL;
+Node* queueHead = NULL;
+
 int main() { 
 
   return 0;
+}
+
+void pushStack(Node* newNode) {
+ 
+}
+
+Node* popStack() {
+ 
+}
+
+void peekStack() {
+ 
+}
+
+enqueueQueue(Node* newNode) {
+ 
+}
+
+Node* dequeueQueue() {
+ 
 }
